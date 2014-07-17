99ab73adc644522192a64c2d694f002669ad8633
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-07-16 23:31:21 -0400

    documented Indent module

--- Text/Luthor/Indent.hs
+++ Text/Luthor/Indent.hs
@@ -1,20 +1,28 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Text.Luthor.Indent (
+    -- * Types
       ParsecIT, ParsecI, IndentState
+    , IndentPolicy(..)
+    -- * Run Indentation-sensitive Parsers
+    , runParserIT, runParserI, runPIT, runPI
+    -- * Parse Indentation
     , mkWs
     , indent, nextline, dedent
+    -- * Read/Write Indentation State
     , isIndentEnabled
     , getIndentDepth
     , withIndentation, withoutIndentation
+    -- * Re-exports and Overrides
+    , module Text.Luthor
     , getState, putState, modifyState
     ) where
 
 import Control.Monad
-import Text.Parsec.Prim (ParsecT, Parsec, Stream, State(..), parserZero)
-import qualified Text.Parsec.Prim as P
-import Text.Luthor.Combinator
-import qualified Text.Parsec.Combinator as P
+import Control.Monad.Identity
+import Text.Luthor hiding (getState, putState, modifyState)
 import Text.Luthor.Syntax
+import qualified Text.Parsec.Prim as P
+import qualified Text.Parsec.Combinator as P
 
 
 -- |Opaque type tracking indentation state.
@@ -22,13 +30,40 @@ data IndentState = IS { _policy :: IndentPolicy
                       , _depth :: [Int]
                       , _enabled :: Bool
                       }
+startIndent :: IndentPolicy -> IndentState
+startIndent policy = IS policy [0] True
 
 -- |Type for Parsec parsers tracking indentation.
 type ParsecIT s u m a = ParsecT s (u, IndentState) m a
 -- |'ParsecIT' over the identity monad.
 type ParsecI s u a = Parsec s (u, IndentState) a
 
+{-| The most general way to run a parser. @runParserIT p state filePath input@
+    runs parser @p@ on the input list of tokens @input@, obtained from source
+    @filePath@ with the initial user state @st@. Indentation is initially
+    enabled and the depth stack begins as @[0]@.
+    The @filePath@ is only used in error messages and may be the empty string.
+    Returns a computation in the underlying monad @m@ that return either a
+    @ParseError@ ('Left') or a value of type @a@ ('Right'). 
+-}
+runParserIT :: Stream s m t => ParsecIT s u m a -> IndentPolicy -> u -> SourceName -> s -> m (Either ParseError a)
+runParserIT p policy u = runParserT p (u, startIndent policy)
 
+{-| As 'runParserIT', but over the Identity monad. -}
+runParserI :: Stream s Identity t => ParsecI s u a -> IndentPolicy -> u -> SourceName -> s -> Either ParseError a
+runParserI p policy u = runParser p (u, startIndent policy)
+
+-- |Shortcut for 'runParserIT'
+runPIT :: Stream s m t => ParsecIT s u m a -> IndentPolicy -> u -> SourceName -> s -> m (Either ParseError a)
+runPIT = runParserIT
+
+-- |Shortcut for 'runParserI'
+runPI :: Stream s Identity t => ParsecI s u a -> IndentPolicy -> u -> SourceName -> s -> Either ParseError a
+runPI = runParserI
+
+
+-- |Parse an indent: as 'dentation' ensuring the result is greater than
+--  the current indentation level. Pushes the indentation depth stack.
 indent :: (Stream s m Char) => ParsecIT s u m ()
 indent = try $ do
     n <- getIndentDepth
@@ -42,6 +77,8 @@ indent = try $ do
     let s' = s { _depth = n' : _depth s }
     P.putState (u, s')
 
+-- |Parse an indent: as 'dentation' ensuring the result is equal to
+--  the current indentation level.
 nextline :: (Stream s m Char) => ParsecIT s u m ()
 nextline = try $ do
     n <- getIndentDepth
@@ -52,6 +89,9 @@ nextline = try $ do
         EQ -> return ()
         GT -> unexpected "indent"
 
+-- |Parse an indent: as 'dentation' ensuring the result is less than
+--  the current indentation level. Pops the indentation depth stack.
+--  If more dedents could be parsed, then no input is consumed.
 dedent :: (Stream s m Char) => ParsecIT s u m ()
 dedent = try $ do
     n <- getIndentDepth
@@ -68,9 +108,12 @@ dedent = try $ do
         else P.putState (u, s')
 
 
+-- |Test if indentation is enabled.
 isIndentEnabled :: (Stream s m t) => ParsecIT s u m Bool
 isIndentEnabled = _enabled . snd <$> P.getState
 
+-- |Obtain the current indentation depth.
+--  Fails if indentation is disabled.
 getIndentDepth :: (Stream s m t) => ParsecIT s u m Int
 getIndentDepth = do
     s <- snd <$> P.getState
@@ -79,6 +122,7 @@ getIndentDepth = do
     if null stack then parserZero else return (head stack)
 
 
+-- |Run the passed parser with indentation enabled.
 withIndentation :: (Stream s m t) => ParsecIT s u m a -> ParsecIT s u m a
 withIndentation p = do
     (u, s) <- P.getState
@@ -92,6 +136,7 @@ withIndentation p = do
         P.putState (u, s')
         return result
 
+-- |Run the passed parser with indentation disabled.
 withoutIndentation :: (Stream s m t) => ParsecIT s u m a -> ParsecIT s u m a
 withoutIndentation p = do
     (u, s) <- P.getState
@@ -124,13 +169,16 @@ mkWs ps = manyOf_ $ ps ++ [blankline]
         try $ lineBreak *> manyOf ps *> lookAhead lineBreak
 
 
+-- |Alternate version of Parsec's @getState@ suited for indentation-sensitive parsers.
 getState :: (Monad m) => ParsecIT s u m u
 getState = fst <$> P.getState
 
+-- |Alternate version of Parsec's @puttState@ suited for indentation-sensitive parsers.
 putState :: (Monad m) => u -> ParsecIT s u m ()
 putState x = void $ P.updateParserState $
     \s@State {stateUser = (_, i)} -> s { stateUser = (x, i) }
 
+-- |Alternate version of Parsec's @modifyState@ suited for indentation-sensitive parsers.
 modifyState :: (Monad m) => (u -> u) -> ParsecIT s u m ()
 modifyState f = void $ P.updateParserState $
     \s@State {stateUser = (u, i)} -> s { stateUser = (f u, i) }
--- test/indent.hs
+++ test/indent.hs
@@ -1,4 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
 import System.IO
 import System.Exit
 
-main = exitFailure
+import Text.Luthor
+import Text.Luthor.Syntax
+import Text.Luthor.Indent
+import qualified Text.Parsec as P
+
+main = do
+    results <- sequence [
+    	False <$ putErrLn "TODO: lots"
+        ]
+    if id `all` results then exitSuccess else exitFailure
+
+
+type Parser = Parsec String ()
+run p = runP p () ""
+
+test :: Bool -> Parser a -> String -> IO Bool
+test expect p input = do
+    case (expect, run (p <* P.eof) input) of
+        (True, Right _) -> return True
+        (False, Left _) -> return True
+        (True, Left err) -> do
+            putErrLn $ "false negative on " ++ show input
+            putErrLn $ show err
+            return False
+        (False, Right _) -> do
+            putErrLn $ "false positive on " ++ show input
+            return False
+
+parses :: (Show a, Eq a) => Parser a -> String -> a -> IO Bool
+parses p input expect = do
+    case run (p <* P.eof) input of
+        Right val | val == expect -> return True
+                  | otherwise -> do
+                        putErrLn $ "incorrect parse on " ++ show input
+                        putErrLn $ "found " ++ show val ++ " expecting " ++ show expect
+                        return False
+        Left err -> do
+            putErrLn $ "false negative on " ++ show input
+            putErrLn $ show err
+            return False
+
+matches = test True
+fails = test False
+
+putErrLn = hPutStrLn stderr
